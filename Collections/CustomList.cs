ff7291a95ab1ad2c97cc8385cc16bdf8d65874e3
Mine3e <minememmedlii@gmail.com>
2024-04-18 00:21:42 +0400

    commit

---
+++ Collections/CustomList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private T[] Items;
+        public int Capacity { get; private set; }
+        public int Count { get; private set; }
+        public T this[int index]
+        {
+            get
+            {
+                return Items[index];
+            }
+            set
+            {
+                if (index< 0 || index >= Count)
+                    throw new IndexOutOfRangeException("index duzgun deyil.");
+                Items[index] = value;
+            }
+        }
+
+        public CustomList()
+        {
+            Items = Array.Empty<T>();
+        }
+
+        public void Add(T item)
+        {
+            Array.Resize(ref Items, Items.Length + 1);
+            Items[^1] = item;
+            Count++;
+        }
+
+        public T Find(Predicate<T> predicate) => Array.Find(Items, predicate);
+
+        public List<T> FindAll(Predicate<T> predicate) => new List<T>(Array.FindAll(Items, predicate));
+
+        public void Remove(T item)
+        {
+            int index = Array.IndexOf(Items, item, 0, Count);
+            if (index >= 0)
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    Items[i] = Items[i + 1];
+                }
+                Count--;
+                Array.Resize(ref Items, Count);
+            }
+        }
+
+
+        public int RemoveAll(Predicate<T> predicate)
+        {
+            T[] newItems = Array.FindAll(Items, item => !predicate(item));
+            int removedCount = Count - newItems.Length;
+            Array.Copy(newItems, Items, newItems.Length);
+            Count = newItems.Length;
+            return removedCount;
+        }
+
+        public void Reverse()
+        {
+            for (int i = 0; i < Count / 2; i++)
+            {
+                T exmp = Items[i];
+                Items[i] = Items[Count - i - 1];
+                Items[Count - i - 1] = exmp;
+            }
+        }
+        public void Sort()
+        {
+            T[] newArray = new T[Count];
+            Array.Copy(Items, newArray, Count);
+            Array.Sort(newArray);
+            Array.Copy(newArray, Items, Count);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return Items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(5);
+            customList.Add(10);
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
+            customList.Remove(5);
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
+            customList.Add(50);
+            customList.Add(45);
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
+            Console.WriteLine(customList.Find(predicate => predicate == 45));
+            Console.WriteLine("----");
+            customList.Reverse();
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----");
+            customList.Sort();
+            foreach (var item in customList)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
